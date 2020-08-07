dd3620406847ab37214e5981f26f7ffa02a8d8d1
samy <samyzhg@qq.com>
2020-08-07 14:06:27 +0800

    修改ask

--- exts/ask.js
+++ exts/ask.js
@@ -3,12 +3,12 @@ module.exports = function askCreator(template = "") {
     {
       type: "input",
       name: "proName",
-      message: "项目名",
+      message: "项目名(en)",
       default: "autorun",
       validate(input) {
         const done = this.async();
         if (input.trim().length === 0) {
-          done("项目名不能为空");
+          done("路径项目名不能为空");
           return;
         }
         done(null, true);
@@ -17,11 +17,11 @@ module.exports = function askCreator(template = "") {
     {
       type: "input",
       name: "moduleName",
-      message: "模块名",
+      message: "模块名(en)",
       validate(input) {
         const done = this.async();
         if (input.trim().length === 0) {
-          done("模块名不能为空");
+          done("路径模块名不能为空");
           return;
         }
         done(null, true);
@@ -30,16 +30,22 @@ module.exports = function askCreator(template = "") {
     {
       type: "input",
       name: "pageName",
-      message: "页面名",
+      message: "页面名(en)",
       default: template,
       validate(input) {
         const done = this.async();
         if (input.trim().length === 0) {
-          done("页面名不能为空");
+          done("路径页面名不能为空");
           return;
         }
         done(null, true);
       },
+    },,
+    {
+      type: "input",
+      name: "name",
+      message: "页面名Title(cn)",
+      default: '添加表单示范',
     },
   ];
 };
